0a55bcca04fbf3d1ee71f2e2890736c7d43d8eae
Ksiren <114107580+Ksiren@users.noreply.github.com>
2022-11-09 18:00:56 +0300

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,4 +1,4 @@
-#include "matrix.h"
+# include "matrix.h"
 
 int main(){
     int l, w, l1, w1;
@@ -7,22 +7,22 @@ int main(){
     Matrix<int>  Mat(l, w);
     cout << "Enter the matrix:\n";
     cin >> Mat;
-    // Mat.matr0[0][0] = 4;
-    // Mat.matr0[1][2] = 6;
-    // Mat.matr0[1][0] = 3;
-    cout << Mat;
-
+    // cout << Mat;
+    
     cout << "Enter the sizes of the second matrix:\n";
     cin >> l1 >> w1;
     Matrix<int>  Mat2(l1, w1);
     cout << "Enter the matrix:\n";
-    // Mat2.matr0[0][0] = 2;
-    // Mat2.matr0[1][1] = 2;
-    // Mat2.matr0[1][0] = 1;
     cin >> Mat2;
-    cout << Mat2;
-
-    cout << "Sum of these matrices:" << endl << Mat + Mat2;
-
-
+    // cout << Mat2;
+    
+    Matrix<int>  Mat_res(l1, w1);
+    cout << "Sum of these matrices:" << endl;
+    Mat_res = Mat + Mat2;
+    cout << Mat_res;
+    
+    cout << "Enter the num by which you want to multiply the matrix:\n";
+    int koef;
+    cin >> koef;
+    cout << Mat_res * koef;
 }
