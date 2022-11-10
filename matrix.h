35aad2e1d0279d21b303f2c7d304358bdeb45fb9
Ksiren <114107580+Ksiren@users.noreply.github.com>
2022-11-10 12:12:13 +0300

    we can mult matrices now

--- matrix.h
+++ matrix.h
@@ -10,28 +10,18 @@ int length, width;
     vector<vector<num_type>> matr0;
 public:
     Matrix(int leng, int wid);
-    
-    // void set_m(vector<vector<num_type>> mat){
-    //     for (int i = 0; i < width; i++){
-    //         for (int j = 0; j < length; j++){
-    //             matr0[i][j] = mat[i][j];
-    //         }
-    //     }
-    // }
-    
-    // void print(){
-    //     cout << "Matrix (" << length << "x" << width << ")" << endl;
-    //     for (int i = 0; i < matr0.size(); ++i) {
-    //         for (int j = 0; j < matr0[i].size(); ++j) {
-    //             cout << matr0[i][j] << ' ';
-    //         } 
-    //         cout << endl;
-    //     }
-    // }
+    // vector trans_mat(Matrix<num_type>&);
     
     const Matrix<num_type> operator+(const Matrix<num_type>&);
     const Matrix<num_type> operator*(num_type);
+    const Matrix<num_type> operator*(const Matrix<num_type>&);
 };
+
+// template<typename num_type>
+// Matrix<num_type>::vector trans_mat(Matrix<num_type>&){
+    
+// }
+
 
 template<typename num_type>
 ostream& operator <<(ostream& out, Matrix<num_type> matr){
@@ -90,4 +80,23 @@ const Matrix<num_type> Matrix<num_type>::operator*(num_type ko) {
         }
     }
     return res_matrix;
+}
+
+template<typename num_type>
+const Matrix<num_type> Matrix<num_type>::operator*(const Matrix<num_type>& matr1){
+    Matrix<num_type> res_matrix(matr1.matr0[0].size(), matr0.size());
+    if (matr0[0].size() != matr1.matr0.size()){
+        cout << "!!!ERROR!!!" << endl << "(wrong sizes)" << endl << matr0[0].size() << " != " << matr1.matr0.size() << endl << "return to the first matrix"<< endl << "|||" << endl << "||" << endl << "|" << endl;
+        return *this;
+    } else{
+        for (int i = 0; i < matr0.size(); i++){
+            for (int j = 0; j < matr1.matr0[0].size(); j++){
+                for (int m = 0; m < matr0[0].size(); ++m){
+                    res_matrix.matr0[i][j] += matr0[i][m] * matr1.matr0[m][j];
+                }
+            }
+        }
+        return res_matrix;
+    }
+    
 }
