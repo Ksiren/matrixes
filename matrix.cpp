1d24020edca4cecec0451013d8900291a19a7bf6
Ksiren <114107580+Ksiren@users.noreply.github.com>
2022-11-11 19:35:16 +0300

    We can transpose matrixes now

--- matrix.cpp
+++ matrix.cpp
@@ -1,28 +1,41 @@
-#include "matrix.h"
+# include "matrix.h"
 
-int main(){
+int main() {
     int l, w, l1, w1;
     cout << "Enter the sizes of the first matrix:\n";
     cin >> l >> w;
     Matrix<int>  Mat(l, w);
     cout << "Enter the matrix:\n";
     cin >> Mat;
-    // Mat.matr0[0][0] = 4;
-    // Mat.matr0[1][2] = 6;
-    // Mat.matr0[1][0] = 3;
-    cout << Mat;
 
     cout << "Enter the sizes of the second matrix:\n";
     cin >> l1 >> w1;
     Matrix<int>  Mat2(l1, w1);
     cout << "Enter the matrix:\n";
-    // Mat2.matr0[0][0] = 2;
-    // Mat2.matr0[1][1] = 2;
-    // Mat2.matr0[1][0] = 1;
     cin >> Mat2;
-    cout << Mat2;
 
-    cout << "Sum of these matrices:" << endl << Mat + Mat2;
+    Matrix<int>  Mat_res(l1, w1);
+    cout << "Sum of these matrices:" << endl;
+    Mat_res = Mat + Mat2;
+    cout << Mat_res;
 
+    cout << "Enter the num by which you want to multiply the matrix:\n";
+    int koef;
+    cin >> koef;
+    Mat_res = Mat_res * koef;
+    cout << Mat_res;
 
+    cout << "Enter the sizes of matrix you want to multiply by:\n";
+    int l2, w2;
+    cin >> l2 >> w2;
+    Matrix<int> Mat3(l2, w2);
+    cout << "Enter the matrix:\n";
+    cin >> Mat3;
+    cout << "Product of matrices:\n";
+    Matrix<int>  Mat_mult(l2, w1);
+    Mat_mult = Mat_res * Mat3;
+    cout << Mat_mult;
+
+    cout << "Transposed matrix:\n";
+    cout << Mat_mult.trans_mat();
 }
--- matrix.h
+++ matrix.h
@@ -4,30 +4,34 @@
 using namespace std;
 
 template<typename num_type>
-class Matrix{
+class Matrix {
 public:
-int length, width;
+    int length, width;
     vector<vector<num_type>> matr0;
 public:
     Matrix(int leng, int wid);
-    // vector trans_mat(Matrix<num_type>&);
-    
+
+    const Matrix<num_type> trans_mat() {
+        Matrix<num_type> res_matrix(matr0.size(), matr0[0].size());
+        for (int i = 0; i < matr0.size(); i++) {
+            for (int j = 0; j < matr0[i].size(); j++) {
+                res_matrix.matr0[j][i] = matr0[i][j];
+            }
+        }
+        return res_matrix;
+    }
+
     const Matrix<num_type> operator+(const Matrix<num_type>&);
     const Matrix<num_type> operator*(num_type);
     const Matrix<num_type> operator*(const Matrix<num_type>&);
 };
 
-// template<typename num_type>
-// Matrix<num_type>::vector trans_mat(Matrix<num_type>&){
-    
-// }
-
 
 template<typename num_type>
-ostream& operator <<(ostream& out, Matrix<num_type> matr){
+ostream& operator <<(ostream& out, Matrix<num_type> matr) {
     out << "Matrix(" << matr.length << "x" << matr.width << ")\n";
-    for (int i = 0; i < matr.matr0.size(); i++){
-        for (int j = 0; j < matr.matr0[i].size(); j++){
+    for (int i = 0; i < matr.matr0.size(); i++) {
+        for (int j = 0; j < matr.matr0[i].size(); j++) {
             out << matr.matr0[i][j] << ' ';
         }
         out << endl;
@@ -36,9 +40,9 @@ ostream& operator <<(ostream& out, Matrix<num_type> matr){
 }
 
 template<typename num_type>
-istream& operator>>(istream& in, Matrix<num_type>& mat){
-    for (int i = 0; i < mat.matr0.size(); i++){
-        for (int j = 0; j < mat.matr0[i].size(); j++){
+istream& operator>>(istream& in, Matrix<num_type>& mat) {
+    for (int i = 0; i < mat.matr0.size(); i++) {
+        for (int j = 0; j < mat.matr0[i].size(); j++) {
             in >> mat.matr0[i][j];
         }
     }
@@ -47,25 +51,26 @@ istream& operator>>(istream& in, Matrix<num_type>& mat){
 
 
 template<typename num_type>
-Matrix<num_type>::Matrix(int leng, int wid){
+Matrix<num_type>::Matrix(int leng, int wid) {
     length = leng;
     width = wid;
     matr0 = vector<vector<num_type>>(width, vector<num_type>(length, 0));
 }
 
 template<typename num_type>
-const Matrix<num_type> Matrix<num_type>::operator+(const Matrix<num_type>& matr1){
+const Matrix<num_type> Matrix<num_type>::operator+(const Matrix<num_type>& matr1) {
     Matrix<num_type> res_matrix(matr0[0].size(), matr0.size());
-    if (matr0.size() != matr1.matr0.size() || matr1.matr0[0].size() != matr0[0].size()){
-        cout << "!!!ERROR!!!" << endl << "(wrong sizes)" << endl << "return to the first matrix"<< endl << "|||" << endl << "||" << endl << "|" << endl;
+    if (matr0.size() != matr1.matr0.size() || matr1.matr0[0].size() != matr0[0].size()) {
+        cout << "!!!ERROR!!!" << endl << "(wrong sizes)" << endl << "return to the first matrix" << endl << "|||" << endl << "||" << endl << "|" << endl;
         return *this;
-    } else{
-        for (int i = 0; i < matr0.size(); i++){
-            for (int j = 0; j < matr0[i].size(); j++){
+    }
+    else {
+        for (int i = 0; i < matr0.size(); i++) {
+            for (int j = 0; j < matr0[i].size(); j++) {
                 res_matrix.matr0[i][j] = matr1.matr0[i][j] + matr0[i][j];
             }
         }
-        
+
     }
     return res_matrix;
 }
@@ -74,8 +79,8 @@ const Matrix<num_type> Matrix<num_type>::operator+(const Matrix<num_type>& matr1
 template<typename num_type>
 const Matrix<num_type> Matrix<num_type>::operator*(num_type ko) {
     Matrix<num_type> res_matrix(matr0[0].size(), matr0.size());
-    for (int i = 0; i < matr0.size(); i++){
-        for (int j = 0; j < matr0[i].size(); j++){
+    for (int i = 0; i < matr0.size(); i++) {
+        for (int j = 0; j < matr0[i].size(); j++) {
             res_matrix.matr0[i][j] = matr0[i][j] * ko;
         }
     }
@@ -83,20 +88,21 @@ const Matrix<num_type> Matrix<num_type>::operator*(num_type ko) {
 }
 
 template<typename num_type>
-const Matrix<num_type> Matrix<num_type>::operator*(const Matrix<num_type>& matr1){
+const Matrix<num_type> Matrix<num_type>::operator*(const Matrix<num_type>& matr1) {
     Matrix<num_type> res_matrix(matr1.matr0[0].size(), matr0.size());
-    if (matr0[0].size() != matr1.matr0.size()){
-        cout << "!!!ERROR!!!" << endl << "(wrong sizes)" << endl << matr0[0].size() << " != " << matr1.matr0.size() << endl << "return to the first matrix"<< endl << "|||" << endl << "||" << endl << "|" << endl;
+    if (matr0[0].size() != matr1.matr0.size()) {
+        cout << "!!!ERROR!!!" << endl << "(wrong sizes)" << endl << matr0[0].size() << " != " << matr1.matr0.size() << endl << "return to the first matrix" << endl << "|||" << endl << "||" << endl << "|" << endl;
         return *this;
-    } else{
-        for (int i = 0; i < matr0.size(); i++){
-            for (int j = 0; j < matr1.matr0[0].size(); j++){
-                for (int m = 0; m < matr0[0].size(); ++m){
+    }
+    else {
+        for (int i = 0; i < matr0.size(); i++) {
+            for (int j = 0; j < matr1.matr0[0].size(); j++) {
+                for (int m = 0; m < matr0[0].size(); ++m) {
                     res_matrix.matr0[i][j] += matr0[i][m] * matr1.matr0[m][j];
                 }
             }
         }
         return res_matrix;
     }
-    
+
 }
